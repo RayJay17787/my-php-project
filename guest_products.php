15fa8b1f15e35abab9f9aac477e545f5d356e8b5
syed ahsan <ahsan.15911_n@iqra.edu.pk>
2025-12-22 20:11:35 +0500

    'new'

--- guest_products.php
+++ guest_products.php
@@ -1,3 +1,23 @@
 <?php
     include 'guestheader.php';
 ?>
+<div class="container p-5 mt-5">
+    <div class="row g-4">
+        <div class="col-lg-3 col-md-4 col-sm-6">
+            <div class="card border shadow-sm h-100">
+                <a class="text-decoration-none text-dark" href="product-details.php?id=1"> 
+                    <img src="images/forza horizon 4 xbox.jpg"
+                        class="card-img-top" alt="Product" style="height: 300px;">
+                    <div class="card-body d-flex flex-column">
+                        <div class="card-title" style="min-height: 48px;">forza horizon  4</div>
+                    </a>
+                    <p class="text-danger fw-bold mb-3">$36.00</p>
+                    <button class="btn btn-danger w-100 mt-auto">
+                        <i class="fa-solid fa-cart-arrow-down"></i> Add to Cart
+                    </button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
